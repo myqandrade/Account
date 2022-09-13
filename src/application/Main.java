3f3ef84cf1d758b8485d96942f34a62380de87d9
Mike Andrade <mike.andrade.oliveira@gmail.com>
2022-09-13 13:22:55 -0300

    Corrigindo bugs e refatorando

--- src/application/Main.java
+++ src/application/Main.java
@@ -15,15 +15,14 @@ public class Main {
 
         System.out.print("Enter account number: ");
         int number = sc.nextInt();
-
         System.out.print("Enter account holder: ");
+        sc.nextLine();
         String holder = sc.nextLine();
-
         System.out.print("Is there an initial deposit (y/n)?");
         char response = sc.next().charAt(0);
 
         if(response == 'y'){
-            System.out.println("Enter initial deposit: ");
+            System.out.print("Enter initial deposit value: ");
             double deposit = sc.nextDouble();
             account = new Account(number, holder, deposit);
         }
@@ -31,14 +30,17 @@ public class Main {
             account = new Account(number, holder);
         }
 
+        System.out.println();
         System.out.println("Account data: ");
         System.out.println(account);
 
+        System.out.println();
         System.out.print("Enter a deposit value: ");
         account.deposit(sc.nextDouble());
         System.out.println("Updated account data: ");
         System.out.println(account);
 
+        System.out.println();
         System.out.print("Enter a withdraw value: ");
         account.withdraw(sc.nextDouble());
         System.out.println("Updated account data: ");
